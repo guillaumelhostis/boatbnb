400ccc089b4505dcefd50d9526c8fd685f0898cd
guillaume lhostis <guillaume.lhostis@free.fr>
2023-06-05 10:20:35 +0200

    created initial seeds for everyone

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,105 @@ require 'faker'
 
 # Supprimer les enregistrements existants
 Cruise.delete_all
+User.delete_all
 
-# Créer 10 croisières avec des données aléatoires
-10.times do
-  Cruise.create(
-    title: Faker::Lorem.sentence(word_count: 3),
-    boat_model: Faker::Vehicle.model,
-    itinerary: Faker::Address.full_address,
-    starting_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
-    ending_date: Faker::Date.between(from: 1.year.from_now, to: 2.years.from_now),
-    description: Faker::Lorem.paragraph(sentence_count: 3),
-    boat_age: Faker::Number.between(from: 1, to: 10),
-    owner_id: Faker::Number.between(from: 1, to: 5),
-    price: Faker::Number.between(from: 350, to: 1000)
-  )
-end
+kiki = User.create(
+  first_name: "Kiki",
+  last_name: "Dee",
+  gender: "Female",
+  date_of_birth: "1995-12-29",
+  email: "kiki@gmail.com",
+  password: "hellokiki",
+  address: "42 bis rue des lilas",
+  postcode: 92160,
+  city: "ANTONY",
+  sailing_experience: "I'm a beginner",
+  description: "Hi, my name is Kiki, i'm Canadian and i love sailing",
+  admin: false
+)
+
+serge = User.create(
+  first_name: "Serge",
+  last_name: "Dupont",
+  gender: "Male",
+  date_of_birth: "1978-10-08",
+  email: "serge@free.fr",
+  password: "serge9092",
+  address: "10 rue de Rome",
+  postcode: 75012,
+  city: "PARIS",
+  sailing_experience: "I'm a pro sailor",
+  description: "Hi, my name is Kiki, i'm Parisian but i'm not annoying",
+  admin: false
+)
+
+User.create(
+  first_name: "Guillaume",
+  last_name: "L'Hostis",
+  gender: "Male",
+  date_of_birth: "1992-09-28",
+  email: "guillaume.lhostis@free.fr",
+  password: "lewagon9092",
+  address: "42 bis rue des roses",
+  postcode: 92160,
+  city: "ANTONY",
+  sailing_experience: "I hate sailing",
+  description: "I'm the admin of Boatbnb",
+  admin: true
+)
+
+kiki_cruise_one = Cruise.create(
+  title: "Voyagez au bout du monde avec Kiki",
+  boat_model: "Outremer 45",
+  itinerary: "Brest - Fort de France",
+  starting_date: "2023-06-10",
+  ending_date: "2023-07-10",
+  description: "Transat des alisées sur un catamaran dernier cri",
+  boat_age: 2,
+  owner_id: kiki[:id],
+  price: 700
+)
+
+kiki_cruise_two = Cruise.create(
+  title: "Les Maldives avec Kiki",
+  boat_model: "Outremer 45",
+  itinerary: "Brest - Les Maldives",
+  starting_date: "2023-09-01",
+  ending_date: "2023-10-10",
+  description: "Ca va secouer",
+  boat_age: 2,
+  owner_id: kiki[:id],
+  price: 600
+)
+
+serge_cruise = Cruise.create(
+  title: "La ballade de Serge",
+  boat_model: "Catana 35",
+  itinerary: "Roscoff- Jersey",
+  starting_date: "2023-06-18",
+  ending_date: "2023-06-20",
+  description: "Pas trop vite mais pas trop doucement non plus",
+  boat_age: 2,
+  owner_id: serge[:id],
+  price: 60
+)
+
+Booking.delete_all
+
+Booking.create(
+  cruise_id: kiki_cruise_one[:id],
+  customer_id: serge[:id],
+  status: "Waiting for approval"
+)
+
+Booking.create(
+  cruise_id: kiki_cruise_two[:id],
+  customer_id: serge[:id],
+  status: "Waiting for approval"
+)
+
+Booking.create(
+  cruise_id: serge_cruise[:id],
+  customer_id: kiki[:id],
+  status: "Waiting for approval"
+)
