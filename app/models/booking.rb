a7048c6dc42371d36ce939407b19bb556d9c0273
guillaume lhostis <guillaume.lhostis@free.fr>
2023-06-05 09:01:09 +0200

    Change Booking Status to only 3 options

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
+  OPTIONS = ["Accepted", "Refused", "Waiting for approval"]
+
   belongs_to :cruise
   belongs_to :customer, class_name: "User"
-
   validates :status, presence: true
+  validates_inclusion_of :status, :in => OPTIONS
 end
